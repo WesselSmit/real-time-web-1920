382f15f40c3d4892fa57f45e1f38cf7fabbe1f3b
Wessel Smit <wrsmit00@gmail.com>
2020-04-17 16:21:21 +0200

    codemirror clientside start

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,9 @@
-console.log('ja')
+const editor = document.getElementById('editor')
+const sourceCode = CodeMirror.fromTextArea(editor, {
+	mode: "xml",
+	theme: "dracula",
+	lineNumbers: true,
+	autoCloseTags: true
+})
+
+document.addEventListener('click', () => console.log(sourceCode))
