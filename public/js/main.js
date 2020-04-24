09e46080b5626fb4d5ddaff572b5e46a12f89897
Wessel Smit <wrsmit00@gmail.com>
2020-04-24 20:13:17 +0200

    added eventHandler with debounce on editor ('change') event

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
+import * as utils from './modules/utils.mjs'
+
 const editor = document.getElementById('editor')
 const language = document.querySelector('[room-language]').getAttribute('room-language')
 const mode = document.querySelector('[room-mode]').getAttribute('room-mode')
@@ -10,4 +12,14 @@ const sourceCode = CodeMirror.fromTextArea(editor, {
 	autoCloseTags: true
 })
 
-document.addEventListener('click', () => console.log(sourceCode.getValue()))
+
+
+
+
+
+
+const debounceTime = 250
+
+sourceCode.on('change', utils.debounce((editor) => {
+	console.log(editor, editor.getValue())
+}, debounceTime))
